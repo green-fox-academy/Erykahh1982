a17ddea631ee31c4536b5062bd89b0db59616a9a
Erika Kovács-Posta <posta.erika.mail@gmail.com>
2018-10-17 11:46:46 +0200

    JavaTasks_04/05/06

---
+++ week-03/day-03/ExpressionAndControlFlow/src/CodingHours.java
@@ -0,0 +1,5 @@
+public class CodingHours {
+    public static void main(String[] args) {
+        
+    }
+}
---
+++ week-03/day-03/ExpressionAndControlFlow/src/IntroduceYourself.java
@@ -0,0 +1,7 @@
+public class IntroduceYourself {
+    public static void main(String[] args) {
+        System.out.println("Erika Kovács-Posta");
+        System.out.println(35);
+        System.out.println(1.61);
+    }
+}
