a6cce4a406a78ca51ac02f305d2c6eef2d21d624
Erika Kovács-Posta <posta.erika.mail@gmail.com>
2018-10-17 11:02:07 +0200

    JavaTasks_HelloWorld_01/02/03

---
+++ week-03/day-03/ExpressionAndControlFlow/src/HelloOthers.java
@@ -0,0 +1,14 @@
+public class HelloOthers {
+    public static void main(String[] args) {
+        System.out.println("Hello Emese!");
+        System.out.println("Hello Edit!");
+        System.out.println("Hello Eszter!");
+        System.out.println("Hello Judit!");
+        System.out.println("Hello Szilvi!");
+        System.out.println("Hello Ági!");
+        System.out.println("Hello Viola!");
+        System.out.println("Hello Zsófi!");
+        System.out.println("Hello Me!");
+    }
+}
+
