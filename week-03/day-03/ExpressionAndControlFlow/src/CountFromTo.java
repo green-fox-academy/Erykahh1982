2726508753b3cad4643863802023bc2e96a2f8f3
Erika Kovács-Posta <posta.erika.mail@gmail.com>
2018-10-19 23:23:58 +0200

    JavaTasks_23/24/25/26/27

---
+++ week-03/day-03/ExpressionAndControlFlow/src/CountFromTo.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class CountFromTo {
+    public static void main(String[] args) {
+        Scanner scannerInput = new Scanner(System.in);
+
+        System.out.println("Please enter a number : ");
+        int userInput1 = scannerInput.nextInt();
+
+        System.out.println("Please enter another number : ");
+        int userInput2 = scannerInput.nextInt();
+
+        if (userInput2 <= userInput1){
+            System.out.println("The second number should be bigger");
+        } else {
+            for ( int a = 0; a < (userInput2 - userInput1); a++) {
+                System.out.println(userInput1 + a);
+            }
+        }
+
+
+
+    }
+}
