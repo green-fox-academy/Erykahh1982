ea25d03f4810dcded3ab27a156c92bfdde1027f7
Erika Kovács-Posta <posta.erika.mail@gmail.com>
2018-10-26 14:50:32 +0200

    CompareLength completed

---
+++ week-04/day-03/FunctionsAndArrays/src/CompareLength.java
@@ -0,0 +1,15 @@
+import java.util.Arrays;
+
+public class CompareLength {
+    public static void main(String[] args) {
+        int[] p1 = {1 ,2 ,3};
+        int[] p2 = {4 ,4};
+
+
+        if (p1.length > p2.length){
+            System.out.println("p1 has more elements" + " : " + Arrays.toString(p1));
+        } else{
+            System.out.println("p2 has more elements" + " : " + Arrays.toString(p2));
+        }
+    }
+}
