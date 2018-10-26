15ba6eccbce07f6170ca274f3285ef9450be23c5
Erika Kovács-Posta <posta.erika.mail@gmail.com>
2018-10-26 09:42:37 +0200

    Finalize excercise Summing

--- week-04/day-03/FunctionsAndArrays/src/Summing.java
+++ week-04/day-03/FunctionsAndArrays/src/Summing.java
@@ -1,19 +1,24 @@
-//Not working properly at the moment//
-
 import java.util.Scanner;
 
 public class Summing {
+    public static void sumAll(int arr[]) {
+        int sum = 0;
+        for (int i = 0; i < arr.length; i++) {
+            sum += arr[i];
+        }
+        System.out.println("The sum of the given numbers is : " + sum);
+    }
 
     public static void main(String[] args) {
-        System.out.println("Please enter an optional number : ");
-        Scanner userInput = new Scanner(System.in);
-        int numberByUser = userInput.nextInt();
-        System.out.println("The sum of the numbers you have entered is : " + sum(numberByUser));
-    }
-    public static int sum(int ... number){
-        int sum = 0;
-        for (int i : number)
-            sum += number.length;
-        return(sum);
+        int num;
+        Scanner input = new Scanner(System.in);
+        System.out.print("How many numbers you want to add together: ");
+        num = input.nextInt();
+        int[] arr2 = new int[num];
+        for (int i = 0; i < arr2.length; i++) {
+            System.out.print("Enter Num" + (i + 1) + ": ");
+            arr2[i] = input.nextInt();
+        }
+        sumAll(arr2);
     }
 }
