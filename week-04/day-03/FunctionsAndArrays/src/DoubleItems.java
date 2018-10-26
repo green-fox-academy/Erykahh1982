acaf81921eebdde7c635860c3062a192e9395a20
Erika Kovács-Posta <posta.erika.mail@gmail.com>
2018-10-26 15:56:51 +0200

    PrintElements_2 and DoubleItems completed

---
+++ week-04/day-03/FunctionsAndArrays/src/DoubleItems.java
@@ -0,0 +1,12 @@
+import java.util.Arrays;
+
+public class DoubleItems {
+    public static void main(String[] args) {
+        int[] numList = {3, 4, 5, 6, 7};
+        System.out.print("The double values of our array are: ");
+        for (int i = 0; i <numList.length ; i++) {
+            numList[i] = numList[i] * 2;
+        }
+        System.out.print(Arrays.toString(numList));
+    }
+}
--- week-04/day-03/FunctionsAndArrays/src/PrintElements.java
+++ week-04/day-03/FunctionsAndArrays/src/PrintElements.java
@@ -1,8 +1,9 @@
 public class PrintElements {
     public static void main(String[] args) {
         int[] numbers = {4, 5, 6, 7};
+        System.out.print("The elements of our array are : ");
         for (int i = 0; i <numbers.length ; i++) {
-            System.out.println(numbers[i]);
+            System.out.print(numbers[i] + ", ");
         }
     }
 }
