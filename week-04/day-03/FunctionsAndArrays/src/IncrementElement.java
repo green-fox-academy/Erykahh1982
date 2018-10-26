8f0338010b0eac7aa6f60e0ec34d2f93aa5a42be
Erika Kovács-Posta <posta.erika.mail@gmail.com>
2018-10-26 15:35:40 +0200

    IncrementElements completed

---
+++ week-04/day-03/FunctionsAndArrays/src/IncrementElement.java
@@ -0,0 +1,8 @@
+public class IncrementElement {
+    public static void main(String[] args) {
+        int[] t = {1, 2, 3, 4, 5};
+        int a = 5;
+        t[2] = t[2] + a;
+        System.out.println("The incremented value of the 3rd element of our array 't' is : " + t[2]);
+    }
+}
