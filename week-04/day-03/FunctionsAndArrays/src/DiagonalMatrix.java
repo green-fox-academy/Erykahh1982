c1426fe4afef8e1be17b82a3365a2b6199de1fd1
Erika Kovács-Posta <posta.erika.mail@gmail.com>
2018-10-28 00:05:58 +0200

    DiagonalMatrix completed

---
+++ week-04/day-03/FunctionsAndArrays/src/DiagonalMatrix.java
@@ -0,0 +1,15 @@
+public class DiagonalMatrix {
+    public static void main(String[] args) {
+        int firstarray [][] = {{1 ,0 ,0 ,0 }, {0 ,1 ,0 ,0 }, {0 ,0 ,1 ,0 }, {0 , 0,0 ,1 }};
+        System.out.println("My first diagonal matrix");
+        display (firstarray);
+    }
+    public static void display (int x [][]){
+        for (int row = 0; row <x.length ; row++) {
+            for (int column = 0; column <x[row].length ; column++) {
+                System.out.print(x[row][column]);
+            }
+            System.out.println();
+        }
+    }
+}
