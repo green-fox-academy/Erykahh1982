384756863b3a27fd0a0e5adfa8415d15c5757e94
Erika Kovács-Posta <posta.erika.mail@gmail.com>
2018-10-31 10:29:49 +0100

    SolarSystem task completed

--- week-05/day-01/StringsAndArrayLists/src/SolarSystem.java
+++ week-05/day-01/StringsAndArrayLists/src/SolarSystem.java
@@ -1,6 +1,19 @@
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class SolarSystem {
 
     public static void main(String[] args) {
+        ArrayList<String> planetList = new ArrayList<String>(Arrays.asList("Mercury", "Venus", "Earth", "Mars", "Jupiter", "Uranus", "Neptune"));
+        System.out.println("Print oringinal Arraylist:");
+        System.out.println(planetList);
+        System.out.println("Print new Arraylist:");
+        System.out.println(putSaturn(planetList));
+    }
 
+    public static ArrayList putSaturn (ArrayList planets){
+        planets.add("Saturn");
+        return planets;
     }
 }
