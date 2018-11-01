ef5a2cf834e19054b6fe12470340095911111776
Erika Kovács-Posta <posta.erika.mail@gmail.com>
2018-11-01 14:41:45 +0100

    Unique deleting previous version, upload new

--- week-04/day-03/FunctionsAndArrays/src/Unique.java
+++ week-04/day-03/FunctionsAndArrays/src/Unique.java
@@ -1,10 +1,43 @@
-import java.util.HashSet;
-import java.util.Set;
+import java.util.Arrays;
 
 public class Unique {
-
     public static void main(String[] args) {
-        int randomList [] = {1 , 2 ,3 ,3 ,4 ,5 ,6 ,6 ,7 ,8 ,9 ,9};
 
+        System.out.println(unique(new int[] {1, 11, 34, 11, 52, 61, 1, 34}));
+
+    }
+    public static String unique(int... numbers) {
+        String uniquePrint ="";
+        int uniqueCount =0;
+
+        for (int i = 0; i < numbers.length; i++) {
+            boolean isUnique = true;
+            for (int j = 0; j < numbers.length; j++) {
+
+                if (numbers[i] == numbers[j] && i > j) {
+                    isUnique = false;
+                }
+            }
+            if (isUnique) {
+                uniqueCount++;
+            }
+        }
+        int[] uniqueArray = new int[uniqueCount];
+        int shift = 0;
+        for (int i = 0; i < uniqueArray.length+shift; i++) {
+            boolean isUnique = true;
+            for (int j = 0; j < numbers.length; j++) {
+                if (numbers[i] == numbers[j] && i > j) {
+                    isUnique = false;
+                }
+            }
+            if (isUnique) {
+                uniqueArray[i-shift] = numbers[i];
+            } else        {
+                shift++;
+            }
+        }
+        uniquePrint = Arrays.toString(uniqueArray);
+        return uniquePrint;
     }
 }
