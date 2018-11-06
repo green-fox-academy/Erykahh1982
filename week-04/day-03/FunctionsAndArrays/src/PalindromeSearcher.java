315f4d952b5c2e837e001d972b890296f1317f2b
Erika Kovács-Posta <posta.erika.mail@gmail.com>
2018-11-06 14:33:10 +0100

    PalindromeSearcher task completed

---
+++ week-04/day-03/FunctionsAndArrays/src/PalindromeSearcher.java
@@ -0,0 +1,40 @@
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class PalindromeSearcher {
+
+    public static void main(String[] args) {
+
+        String word = "racecar";
+        boolean palindrome = checkPalindrome(word);
+        System.out.println("If the entered word is palindrome or not : " + palindrome);
+        System.out.println(findPalindromes(word));
+    }
+    public static boolean checkPalindrome (String string){
+
+        char[] wordArray = string.toCharArray();
+        //System.out.println(Arrays.toString(wordArray));
+        for (int i = 0; i < wordArray.length / 2 ; i++) {
+            if(wordArray[i] == wordArray[wordArray.length - 1 - i]){
+            } else {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static ArrayList<String> findPalindromes (String originalWord){
+        ArrayList<String> output = new ArrayList();
+
+        for (int i = 0; i <= originalWord.length() - 1; i++) {
+            for (int j = i + 2; j <= originalWord.length() - 1; j++) {
+                String partString = originalWord.substring(i, j + 1);
+                if (checkPalindrome(partString) == true ){
+                    output.add(partString);
+                }
+            }
+        }
+        return output;
+    }
+}
---
+++ week-06/day-01/DrawingAndGraphics/src/PurpleSteps3D.java
@@ -0,0 +1,51 @@
+import javax.swing.*;
+
+import java.awt.*;
+
+import static javax.swing.JFrame.EXIT_ON_CLOSE;
+
+public class PurpleSteps3D {
+    public static void mainDraw(Graphics graphics) {
+        int position = 10;
+        for (int i = 0; i < 20; i++) {
+            int side = 10;
+            drawSquare(g, side, position);
+            position = side + position;
+        }
+    }
+
+    public static void drawSquare (Graphics square, int side, int position) {
+        square.setColor(Color.black);
+        square.drawRect(position,position, side, side);
+        square.setColor(Color.magenta);
+        square.fillRect(position+1,position+1,side-1,side-1);
+    }
+
+    public static void position (Graphics square3D, )
+
+
+    }
+
+    // Don't touch the code below
+    static int WIDTH = 320;
+    static int HEIGHT = 320;
+
+    public static void main(String[] args) {
+        JFrame jFrame = new JFrame("Drawing");
+        jFrame.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        ImagePanel panel = new ImagePanel();
+        panel.setPreferredSize(new Dimension(WIDTH, HEIGHT));
+        jFrame.add(panel);
+        jFrame.setLocationRelativeTo(null);
+        jFrame.setVisible(true);
+        jFrame.pack();
+    }
+
+    static class ImagePanel extends JPanel {
+        @Override
+        protected void paintComponent(Graphics graphics) {
+            super.paintComponent(graphics);
+            mainDraw(graphics);
+        }
+    }
+}
