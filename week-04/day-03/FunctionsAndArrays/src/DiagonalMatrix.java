262e4725b7fd190e89693ac3554106023a30f43a
Erika Kovács-Posta <posta.erika.mail@gmail.com>
2018-11-21 14:18:07 +0100

    DiagonalMatrix style revision completed

--- week-04/day-03/FunctionsAndArrays/src/DiagonalMatrix.java
+++ week-04/day-03/FunctionsAndArrays/src/DiagonalMatrix.java
@@ -2,14 +2,18 @@ public class DiagonalMatrix {
 
     public static void main(String[] args) {
 
-        int firstarray [][] = {{1 ,0 ,0 ,0 }, {0 ,1 ,0 ,0 }, {0 ,0 ,1 ,0 }, {0 , 0,0 ,1 }};
+        int firstarray[][] = {
+            {1, 0, 0, 0},
+            {0, 1, 0, 0},
+            {0, 0, 1, 0},
+            {0, 0, 0, 1}};
         System.out.println("My first diagonal matrix");
-        display (firstarray);
+        display(firstarray);
     }
 
-    public static void display (int x [][]){
-        for (int row = 0; row <x.length ; row++) {
-            for (int column = 0; column <x[row].length ; column++) {
+    public static void display(int x[][]) {
+        for (int row = 0; row < x.length; row++) {
+            for (int column = 0; column < x[row].length; column++) {
                 System.out.print(x[row][column]);
             }
             System.out.println();
