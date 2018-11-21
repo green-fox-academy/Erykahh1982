05965d77d543ac77109cab8221a28135ed7915b6
Erika Kovács-Posta <posta.erika.mail@gmail.com>
2018-11-21 11:24:15 +0100

    Sharpie task completed

---
+++ week-08/day-01/src/Sharpie.java
@@ -0,0 +1,51 @@
+public class Sharpie {
+  private String color;
+  private float width;
+  private float inkAmount;
+
+  private Sharpie(){
+  }
+
+  public Sharpie(String color, float width){
+    this.inkAmount = 100;
+    this.color = color;
+    this.width = width;
+
+  }
+  public void use(){
+    inkAmount--;
+  }
+
+  @Override
+  public String toString() {
+    return "Sharpie{" +
+        "color='" + color + '\'' +
+        ", width=" + width +
+        ", inkAmount=" + inkAmount +
+        '}';
+  }
+
+  public void setColor(String color) {
+    this.color = color;
+  }
+
+  public void setWidth(float width) {
+    this.width = width;
+  }
+
+  public void setInkAmount(float inkAmount) {
+    this.inkAmount = inkAmount;
+  }
+
+  public String getColor() {
+    return color;
+  }
+
+  public float getWidth() {
+    return width;
+  }
+
+  public float getInkAmount() {
+    return inkAmount;
+  }
+}
---
+++ week-08/day-01/src/SharpieMain.java
@@ -0,0 +1,16 @@
+public class SharpieMain {
+  public static void main(String[] args) {
+    Sharpie sharpie01 = new Sharpie("blue",10f);
+    Sharpie sharpie02 = new Sharpie("yellow",15f);
+    Sharpie sharpie03 = new Sharpie("red", 78f);
+
+    sharpie01.use();
+    sharpie02.use();
+    sharpie03.use();
+
+    System.out.println(sharpie01);
+    System.out.println(sharpie02);
+    System.out.println(sharpie03);
+
+  }
+}
