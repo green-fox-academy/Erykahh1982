0f18bb06bc1d302fe4d03bea04339cc0389bd193
Erika Kovács-Posta <posta.erika.mail@gmail.com>
2018-12-04 01:35:26 +0100

    feat(GFA Organization):Cohort class created

---
+++ week-10/day-01/GreenFoxOrganization/src/Cohort.java
@@ -0,0 +1,51 @@
+import java.util.ArrayList;
+
+public class Cohort {
+  private String name;
+  private ArrayList<Student> students;
+  private ArrayList<Mentor> mentors;
+
+  public Cohort(String name) {
+    this.name = name;
+    students = new ArrayList<>();
+    mentors = new ArrayList<>();
+  }
+
+  public void info() {
+    System.out.println("The " + name + "cohort has " + students.size() + "students and " + mentors.size() + " mentors");
+  }
+
+  public void addMentor(Mentor mentor) {
+    mentors.add(mentor);
+  }
+
+  public void addStudent(Student student) {
+    students.add(student);
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public ArrayList<Student> getStudents() {
+    return students;
+  }
+
+  public void setStudents(ArrayList<Student> students) {
+    this.students = students;
+  }
+
+  public ArrayList<Mentor> getMentors() {
+    return mentors;
+  }
+
+  public void setMentors(ArrayList<Mentor> mentors) {
+    this.mentors = mentors;
+  }
+
+}
+
