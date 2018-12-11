7be8a346a53670bf5105306c98d4c7250cc70ec3
Erika Kovács-Posta <posta.erika.mail@gmail.com>
2018-12-11 12:36:08 +0100

    feat(Comparable): task completed

--- week-11/day-02/Comparable/src/Main.java
+++ week-11/day-02/Comparable/src/Main.java
@@ -14,13 +14,50 @@ public class Main {
     dominoes.add(new Domino(2, 4));
     dominoes.add(new Domino(7, 1));
 
+    System.out.println("Printing the original domino list");
+    System.out.println(dominoes);
+
     Collections.sort(dominoes, new Comparator<Domino>() {
       @Override
       public int compare(Domino o1, Domino o2) {
         return o1.getLeftSide() - o2.getLeftSide();
       }
     });
+    System.out.println();
+    System.out.println("Printing the sorted domino list");
     System.out.println(dominoes);
+    System.out.println();
+
+    ArrayList<Thing> things = new ArrayList<>();
+
+    Thing getUp = new Thing("Get up");
+    getUp.complete();
+    things.add(getUp);
+
+    Thing washMyTeeth = new Thing("Wash my teeth");
+    washMyTeeth.complete();
+    things.add(washMyTeeth);
+
+    Thing eatBreakfast = new Thing("Eat breakfast");
+    things.add(eatBreakfast);
+
+    Thing packTheBagpack = new Thing("Pack the backpack");
+    things.add(packTheBagpack);
+
+    Thing dressUp = new Thing("Dress up");
+    dressUp.complete();
+    things.add(dressUp);
+
+    Thing leaveTheHouse = new Thing("Leave the house");
+    leaveTheHouse.complete();
+    things.add(leaveTheHouse);
+
+    System.out.println("Printing the original Things list");
+    System.out.println(things);
+    System.out.println();
+    Collections.sort(things);
+    System.out.println("Printing the sorted Things list");
+    System.out.println(things);
   }
 
 }
--- week-11/day-02/Comparable/src/Thing.java
+++ week-11/day-02/Comparable/src/Thing.java
@@ -1,7 +1,7 @@
 /**
  * Created by aze on 2017.03.29..
  */
-public class Thing {
+public class Thing implements Comparable<Thing> {
   private String name;
   private boolean completed;
 
@@ -16,6 +16,31 @@ public class Thing {
   @Override
   public String toString() {
     return (completed ? "[x] " : "[ ] ") + name;
+  }
+
+  @Override
+  public int compareTo(Thing otherThing) {
+    if (!completed) {
+      if (otherThing.isCompleted()) {
+        return -1;
+      } else {
+        return name.compareTo(otherThing.getName());
+      }
+    } else {
+      if (!otherThing.isCompleted()) {
+        return 1;
+      } else {
+        return name.compareTo(otherThing.getName());
+      }
+    }
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public boolean isCompleted() {
+    return completed;
   }
 
 }
