4d00702838f62ab622eff63874bc3ec359b092d3
Erika Kovács-Posta <posta.erika.mail@gmail.com>
2018-12-13 00:55:12 +0100

    test(Sum):created unit test for an ArrayList that is null

--- week-11/day-04/Sum/Tests/SumTest.java
+++ week-11/day-04/Sum/Tests/SumTest.java
@@ -16,7 +16,7 @@ public class SumTest {
   }
 
   @Test
-  public void sumNumbers() {
+  public void sumNumbersOriginalMultipleElement() {
     ArrayList<Integer> listOfNumbers = new ArrayList<>(Arrays.asList(5, 10, 15, 20, 25));
 
     int expectedSumResult = 75;
@@ -40,6 +40,16 @@ public class SumTest {
     ArrayList<Integer> listOfNumbers = new ArrayList<>(Arrays.asList(1));
 
     int expectedSumResult = 1;
+    int calculatedSumResult = sum.sumNumbers(listOfNumbers);
+
+    assertEquals(expectedSumResult,calculatedSumResult);
+  }
+
+  @Test
+  public void sumNumbersWithNull() {
+    ArrayList<Integer> listOfNumbers = null;
+
+    int expectedSumResult = 0;
     int calculatedSumResult = sum.sumNumbers(listOfNumbers);
 
     assertEquals(expectedSumResult,calculatedSumResult);
