0c78970bb4ac08b605f8ea67a28aa805d5059975
Erika Kovács-Posta <posta.erika.mail@gmail.com>
2018-12-13 10:27:52 +0100

    test(Anagram):unit test created for checkAnagramWithSameStringInput

---
+++ week-11/day-04/Anagram/Tests/AnagramTest.java
@@ -0,0 +1,27 @@
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class AnagramTest {
+
+  private static Anagram anagram;
+
+  @BeforeClass
+  public static void init(){
+    anagram = new Anagram();
+  }
+
+  @Test
+  public void checkAnagramWithSameStringInput() {
+
+    String s1 = "god";
+    String s2 = "dog";
+
+    boolean expectedResult = true;
+    boolean calculatedResult = anagram.checkAnagram(s1, s2);
+
+    assertEquals(expectedResult, calculatedResult);
+
+  }
+}
---
+++ week-11/day-04/Anagram/src/Anagram.java
@@ -0,0 +1,31 @@
+import java.util.Arrays;
+
+public class Anagram {
+
+    public boolean checkAnagram(String s1, String s2) {
+
+    char[] s1Array = s1.toCharArray();
+    char[] s2Array = s2.toCharArray();
+
+    for (int i = 0; i < s1Array.length - 1; i++) {
+      for (int j = i + 1; j < s1Array.length; j++) {
+        if (s1Array[i] > s1Array[j]) {
+          char Sort1 = s1Array[j];
+          s1Array[j] = s1Array[i];
+          s1Array[i] = Sort1;
+        }
+      }
+    }
+    for (int k = 0; k < s2Array.length - 1; k++) {
+      for (int l = k + 1; l < s2Array.length; l++) {
+        if (s2Array[k] > s2Array[l]) {
+          char Sort2 = s2Array[l];
+          s2Array[l] = s2Array[k];
+          s2Array[k] = Sort2;
+        }
+      }
+    }
+    boolean equal = Arrays.equals(s1Array, s2Array);
+    return equal;
+  }
+}
