343b7fbc9c7dfae16d8ec4e5d69746d73b8fc19a
Erika Kovács-Posta <posta.erika.mail@gmail.com>
2018-12-13 10:37:37 +0100

    test(Anagram):unit test re-created for checkAnagramWithDifferentStringInput

--- week-11/day-04/Anagram/Tests/AnagramTest.java
+++ week-11/day-04/Anagram/Tests/AnagramTest.java
@@ -27,12 +27,11 @@ public class AnagramTest {
   @Test
   public void checkAnagramWithDifferentStringInput() {
 
-    String s1 = "one";
+    String s1 = "god";
     String s2 = "dog";
 
-    boolean expectedResult = true;
     boolean calculatedResult = anagram.checkAnagram(s1, s2);
 
-    assertEquals(expectedResult, calculatedResult);
+    assertTrue(calculatedResult);
   }
 }
