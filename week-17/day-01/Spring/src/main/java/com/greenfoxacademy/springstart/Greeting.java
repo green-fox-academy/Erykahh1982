c10eac4c9b0ea4c5489d8daa708bbca2c956c9bc
Erika Kovács-Posta <posta.erika.mail@gmail.com>
2019-02-04 22:50:11 +0100

    feat(Hello World REST App):created controllers package, HelloRESTController and Greeting classes along with a greet() method, app result on localhost8080/greeting is as per expected

---
+++ week-17/day-01/Spring/src/main/java/com/greenfoxacademy/springstart/Greeting.java
@@ -0,0 +1,24 @@
+package com.greenfoxacademy.springstart;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+
+public class Greeting {
+
+  public long id;
+  public String content;
+
+  public Greeting(long id, String content) {
+    this.id = id;
+    this.content = content;
+  }
+
+
+
+  public long getId() {
+    return id;
+  }
+
+  public String getContent() {
+    return content;
+  }
+}
