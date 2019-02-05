1844ad681d653f4719bb5cc43e075d6ecc7b4a02
Erika Kovács-Posta <posta.erika.mail@gmail.com>
2019-02-06 00:03:59 +0100

    feat(Library):created index.html and GET/books endpoint

--- week-17/day-02/Thymeleaf/src/main/java/com/greenfoxacademy/thymeleafstart/controllers/BookController.java
+++ week-17/day-02/Thymeleaf/src/main/java/com/greenfoxacademy/thymeleafstart/controllers/BookController.java
@@ -2,6 +2,10 @@ package com.greenfoxacademy.thymeleafstart.controllers;
 
 import com.greenfoxacademy.thymeleafstart.Book;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -14,6 +18,18 @@ public class BookController {
   public BookController() {
     books.add(new Book("Cat's Cradle", "Kurt Vonnegut", 1963));
     books.add(new Book("Do Androids Dream of Electric Sheep?", "Philip K. Dick", 1968));
+  }
+
+  @RequestMapping(path = "/books", method = RequestMethod.GET)    //showBooks() with RequestMapping annotation
+  public String showBooks(Model model) {
+    model.addAttribute("books", books);
+    return "index";
+  }
+
+  @GetMapping("/books")                                           //showBooks() with GetMapping annotation
+  public String showBooks2(Model model) {
+    model.addAttribute("books", books);
+    return "index";
   }
 
 }
