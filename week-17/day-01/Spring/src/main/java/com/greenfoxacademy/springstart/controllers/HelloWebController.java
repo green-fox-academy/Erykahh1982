1aba333924cac378f53367e431e35165e4734d39
Erika Kovács-Posta <posta.erika.mail@gmail.com>
2019-02-05 12:40:24 +0100

    refactor(Hello World Web App) some minor changes have been made and added some comments to explain the code

--- week-17/day-01/Spring/src/main/java/com/greenfoxacademy/springstart/controllers/HelloWebController.java
+++ week-17/day-01/Spring/src/main/java/com/greenfoxacademy/springstart/controllers/HelloWebController.java
@@ -4,13 +4,27 @@ import com.greenfoxacademy.springstart.Greeting;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.concurrent.atomic.AtomicLong;
 
 @Controller
 public class HelloWebController {
 
-  @RequestMapping("/web/greeting")    // http://localhost:8080/web/greeting will show "Hello, Erika!"
+  private AtomicLong countWebsiteLoad = new AtomicLong();
+
+  @RequestMapping("/web/greeting")  // http://localhost:8080/web/greeting will show "Hello, Erika!"
   public String greeting(Model model) {
     model.addAttribute("username", " Erika");
-    return "greeting";
+    return "greeting";   //I write "greeting" here, as my html template name is greeting.html , the program looks for the nem of the html file
   }
+
+//  @RequestMapping("/web/greeting")
+//  // http://localhost:8080/web/greeting will show "greeting" on the screen
+//  @ResponseBody             if I add annotation to this request mapping, the exact String "greeting" will appear on the website
+//  public String greeting(Model model) {
+//    model.addAttribute("username", " Erika");
+//    return "greeting";
+//  }
+
 }
