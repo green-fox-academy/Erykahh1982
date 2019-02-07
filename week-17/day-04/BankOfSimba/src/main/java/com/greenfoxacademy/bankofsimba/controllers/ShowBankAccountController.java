b66e64824ab4440c4eac14e901e63e50c3ec6995
Erika Kovács-Posta <posta.erika.mail@gmail.com>
2019-02-07 13:21:49 +0100

    feat(BankOfSimba):initialize project; created controllers and model packages, created BankAccount class and establish show.html frame and ShowBankAccountController class

---
+++ week-17/day-04/BankOfSimba/src/main/java/com/greenfoxacademy/bankofsimba/controllers/ShowBankAccountController.java
@@ -0,0 +1,12 @@
+package com.greenfoxacademy.bankofsimba.controllers;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class ShowBankAccountController {
+
+  public String showAnimals(){
+
+    return "show";
+  }
+}
