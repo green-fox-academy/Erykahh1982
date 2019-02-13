00223447be047f198b94b52588eeb57650bfd493
Erika Kovács-Posta <posta.erika.mail@gmail.com>
2019-02-13 01:56:44 +0100

    feat(Library)EndPoints for checkBookById and addBook created along with the corresponding html files

--- week-18/day-02/Library/src/main/java/com/greenfoxacademy/libraryapp/controllers/BookController.java
+++ week-18/day-02/Library/src/main/java/com/greenfoxacademy/libraryapp/controllers/BookController.java
@@ -3,43 +3,64 @@ package com.greenfoxacademy.libraryapp.controllers;
 import com.greenfoxacademy.libraryapp.Book;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 
 @Controller
 public class BookController {
 
-  private ArrayList<Book>listOfBooks = new ArrayList<>();
+  private ArrayList<Book> listOfBooks = new ArrayList<>();
 
-  public BookController(){
+  public BookController() {
     listOfBooks.add(new Book("Michelle Obama", "Így lettem", 2018));
     listOfBooks.add(new Book("Kepes András", "Istenek és Emberek", 2018));
-    listOfBooks.add(new Book("Jane Austin", "Büszkeség és balítélet",1813 ));
-    listOfBooks.add(new Book("Bram Stoker", "Drakula",1897 ));
-    listOfBooks.add(new Book("Ernest Miller Hemingway", "Az öreg halász és a tenger",1952));
+    listOfBooks.add(new Book("Jane Austin", "Büszkeség és balítélet", 1813));
+    listOfBooks.add(new Book("Bram Stoker", "Drakula", 1897));
+    listOfBooks.add(new Book("Ernest Miller Hemingway", "Az öreg halász és a tenger", 1952));
   }
 
   @RequestMapping(value = "/books", method = RequestMethod.GET)
-  public String listBooks(Model model){
+  public String listBooks(Model model) {
     model.addAttribute("books", listOfBooks);
     return "displaybooks";
   }
   ///books és /books2 ugyan azt eredményezi @RequestMapping-el és @GetMapping-el is
 
   @GetMapping("/books2")
-  public String listBooks2(Model model){
+  public String listBooks2(Model model) {
     model.addAttribute("books", listOfBooks);
     return "displaybooks2";
   }
 
-  public ArrayList<Book> getListOfBooks() {
-    return listOfBooks;
+  @RequestMapping(value = "/books/{id}/details", method = RequestMethod.GET)
+  public String showBookById(@RequestParam(name = "id") Integer id, Model model) {
+    Book bookById = null;
+
+    for (Book book : listOfBooks) {
+      if (book.getId() == id) {
+        bookById = book;
+      }
+    }
+
+    if (bookById != null) {
+      model.addAttribute("book", bookById);
+    } else {
+      model.addAttribute("error", "No book found with such ID");
+    }
+    return "showdetails";
   }
 
-  public void setListOfBooks(ArrayList<Book> listOfBooks) {
-    this.listOfBooks = listOfBooks;
+  @RequestMapping(value = "books/add", method = RequestMethod.GET)
+  public String addBook(@ModelAttribute(name = "book") Book book, Model model) {
+    model.addAttribute("book", book);
+    return "addbook";
+  }
+
+  @RequestMapping(value = "books/add", method = RequestMethod.POST)
+  public String addBookToList(Model model, Book book){
+    listOfBooks.add(book);
+    model.addAttribute("book", book);
+    return "redirect:/books";
   }
 }
