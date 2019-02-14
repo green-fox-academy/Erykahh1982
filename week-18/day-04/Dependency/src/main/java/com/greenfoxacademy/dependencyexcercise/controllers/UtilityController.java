f060ab62420914ca48430a8b04244bae0886eb2d
Erika Kovács-Posta <posta.erika.mail@gmail.com>
2019-02-14 14:35:12 +0100

    feat(ColoredBackground):modified endpoint /useful/colored and the coloredwebsite.html, not yet working properly...

--- week-18/day-04/Dependency/src/main/java/com/greenfoxacademy/dependencyexcercise/controllers/UtilityController.java
+++ week-18/day-04/Dependency/src/main/java/com/greenfoxacademy/dependencyexcercise/controllers/UtilityController.java
@@ -4,24 +4,27 @@ import com.greenfoxacademy.dependencyexcercise.services.UtilityService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.Banner;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class UtilityController {
 
+  private UtilityService utilityService;
+
   @Autowired
   public UtilityController() {
   }
 
   @RequestMapping(value = "/useful", method = RequestMethod.GET)
-  public String renderUsefulPage(){
+  public String renderUsefulPage() {
     return "utilities";
   }
 
   @RequestMapping(value = "/useful/colored", method = RequestMethod.GET)
-  public String loadColoredWedSite() {
-
+  public String loadColoredWedSite(Model model) {
+    model.addAttribute("color", utilityService.randomColor());
     return "coloredwebsite";
   }
 }
