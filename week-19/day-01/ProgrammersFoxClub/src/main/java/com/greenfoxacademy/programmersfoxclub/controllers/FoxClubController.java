174aea8c7bd9cd7e2d5c430609abe83025fbde52
Erika Kovács-Posta <posta.erika.mail@gmail.com>
2019-02-18 11:58:47 +0100

    feat(ProgrammersFoxClub):added endpoint(POST) /login, modified index.html to show dinamic parameter name coming from the Form

--- week-19/day-01/ProgrammersFoxClub/src/main/java/com/greenfoxacademy/programmersfoxclub/controllers/FoxClubController.java
+++ week-19/day-01/ProgrammersFoxClub/src/main/java/com/greenfoxacademy/programmersfoxclub/controllers/FoxClubController.java
@@ -1,18 +1,27 @@
 package com.greenfoxacademy.programmersfoxclub.controllers;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 public class FoxClubController {
 
   @GetMapping("/main")
-  public String renderMainPage(){
+  public String renderMainPage(@RequestParam(name = "name") String name, Model model) {
+    model.addAttribute("name", name);
     return "index";
   }
 
   @GetMapping("/login")
-  public String renderLoginPage(){
+  public String renderLoginPage() {
     return "login";
+  }
+
+  @RequestMapping(value = "/login", method = RequestMethod.POST)
+  public String postLoginForm(@RequestParam(name = "name") String name, Model model) {
+    model.addAttribute("name", name);
+
+    return "redirect:/main/?name="+ name;
   }
 }
