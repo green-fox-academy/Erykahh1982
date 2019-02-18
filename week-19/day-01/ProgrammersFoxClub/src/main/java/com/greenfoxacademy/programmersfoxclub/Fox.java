c6e78d667be7074abd8a29c8a4cf9dd9f795073e
Erika Kovács-Posta <posta.erika.mail@gmail.com>
2019-02-18 13:23:09 +0100

    feat(ProgrammersFoxClub):made small changes on both html files and created Fox class

---
+++ week-19/day-01/ProgrammersFoxClub/src/main/java/com/greenfoxacademy/programmersfoxclub/Fox.java
@@ -0,0 +1,11 @@
+package com.greenfoxacademy.programmersfoxclub;
+
+import java.util.ArrayList;
+
+public class Fox {
+
+  private String name;
+  private ArrayList<String>listOfTricks;
+  private String food;
+  private String drink;
+}
