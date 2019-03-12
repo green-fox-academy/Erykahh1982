6b3f95e994400d4437bb202c5ae44b7969ac0a77
Erika Kovács-Posta <posta.erika.mail@gmail.com>
2019-03-12 14:00:10 +0100

    feat(listingtodos)task completed

--- week-22/day-02/src/main/java/com/greenfoxacademy/springstart/listingtodos/controller/TodoController.java
+++ week-22/day-02/src/main/java/com/greenfoxacademy/springstart/listingtodos/controller/TodoController.java
@@ -1,11 +1,14 @@
 package com.greenfoxacademy.springstart.listingtodos.controller;
 
+import com.greenfoxacademy.springstart.listingtodos.model.Todo;
 import com.greenfoxacademy.springstart.listingtodos.repository.TodoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.ArrayList;
 
 @Controller
 @RequestMapping("/todo")
@@ -15,8 +18,12 @@ public class TodoController {
   private TodoRepository todoRepository;
 
   @GetMapping("/list")
-  @ResponseBody
-  public String list(){
-    return "This is my first Todo";
+  public String list(Model model) {
+
+    ArrayList<Todo>todos = new ArrayList<>();
+    todoRepository.findAll().forEach(todos::add);
+    model.addAttribute("models", todos);
+
+    return "todolist";
   }
 }
--- week-22/day-02/src/main/java/com/greenfoxacademy/springstart/listingtodos/repository/TodoRepository.java
+++ week-22/day-02/src/main/java/com/greenfoxacademy/springstart/listingtodos/repository/TodoRepository.java
@@ -2,6 +2,8 @@ package com.greenfoxacademy.springstart.listingtodos.repository;
 
 import com.greenfoxacademy.springstart.listingtodos.model.Todo;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface TodoRepository extends CrudRepository<Todo, Long> {
 }
