7d158b29e4fe3c3f71f700a5312d59fdc2304d44
Erika Kovács-Posta <posta.erika.mail@gmail.com>
2019-03-13 10:59:50 +0100

    feat(ListingTodosFomMySQL):able to list only items that are uncompleted via @RequestParam

--- week-22/day-03/ConnectionWithMySQL/src/main/java/com/greenfoxacademy/connectionwithmysql/controller/TodosController.java
+++ week-22/day-03/ConnectionWithMySQL/src/main/java/com/greenfoxacademy/connectionwithmysql/controller/TodosController.java
@@ -2,11 +2,13 @@ package com.greenfoxacademy.connectionwithmysql.controller;
 
 import com.greenfoxacademy.connectionwithmysql.model.Todo;
 import com.greenfoxacademy.connectionwithmysql.repository.TodoRepository;
+import com.greenfoxacademy.connectionwithmysql.service.TodoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.ArrayList;
 
@@ -15,24 +17,41 @@ import java.util.ArrayList;
 public class TodosController {
 
   private TodoRepository todoRepository;
+  private TodoService todoService;
 
   @Autowired
-  public TodosController(TodoRepository todoRepository) {
+  public TodosController(TodoRepository todoRepository, TodoService todoService) {
     this.todoRepository = todoRepository;
+    this.todoService = todoService;
   }
 
   @GetMapping("/list")
   public String list(Model model) {
 
-    ArrayList<Todo> todos = new ArrayList<>();
-    todoRepository.findAll().forEach(todos::add);
-    model.addAttribute("models", todos);
-
+    model.addAttribute("models", todoService.listAllItems());
     return "todolist";
   }
 
-  @GetMapping("/")
-  public String list2() {
-    return "redirect:/todo/list";
+  @GetMapping("/ver1/")
+  public String list2(Model model, @RequestParam(required = false) boolean isActive) {
+    ArrayList<Todo> todos;
+    if (isActive) {
+      todos = todoService.listDoneItems();
+    } else {
+      todos = todoService.listAllItems();
+    }
+    model.addAttribute("models", todos);
+    return "todolist";
+  }
+
+  @GetMapping("/ver2/")
+  public String list3(Model model, @RequestParam(required = false) boolean isActive) {
+    if (isActive) {
+      model.addAttribute("models", todoService.listDoneItems());
+    } else {
+      model.addAttribute("models", todoService.listAllItems());
+    }
+    return "todolist";
+
   }
 }
---
+++ week-22/day-03/ConnectionWithMySQL/src/main/java/com/greenfoxacademy/connectionwithmysql/service/TodoService.java
@@ -0,0 +1,38 @@
+package com.greenfoxacademy.connectionwithmysql.service;
+
+import com.greenfoxacademy.connectionwithmysql.model.Todo;
+import com.greenfoxacademy.connectionwithmysql.repository.TodoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+
+@Service
+public class TodoService {
+
+  private TodoRepository todoRepository;
+
+  @Autowired
+  public TodoService(TodoRepository todoRepository) {
+    this.todoRepository = todoRepository;
+  }
+
+  public ArrayList<Todo> listAllItems() {
+    ArrayList<Todo> todos = new ArrayList<>();
+    todoRepository.findAll().forEach(todos::add);
+    return todos;
+  }
+
+  public ArrayList<Todo> listDoneItems() {
+    ArrayList<Todo> todos = listAllItems();
+    ArrayList<Todo> uncompletedTodos = new ArrayList<>();
+
+    for (Todo todo : todos) {
+      if (!todo.isDone()) {
+        uncompletedTodos.add(todo);
+      }
+    }
+    return uncompletedTodos;
+  }
+
+}
