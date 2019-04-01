b9396b006405a577117b98384647894f03b9bec0
Erika Kovács-Posta <posta.erika.mail@gmail.com>
2019-04-01 10:22:37 +0200

    feat(REST):task1 completed

---
+++ week-24/REST/src/main/java/com/greenfoxacademy/resttasks/ResttasksApplication.java
@@ -0,0 +1,13 @@
+package com.greenfoxacademy.resttasks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResttasksApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(ResttasksApplication.class, args);
+  }
+
+}
---
+++ week-24/REST/src/test/java/com/greenfoxacademy/resttasks/ResttasksApplicationTests.java
@@ -0,0 +1,16 @@
+package com.greenfoxacademy.resttasks;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ResttasksApplicationTests {
+
+  @Test
+  public void contextLoads() {
+  }
+
+}
