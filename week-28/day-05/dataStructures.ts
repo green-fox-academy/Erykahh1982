1d1d1822bd174a80237fd5cfd6992dab83ea869d
Erika Kovács-Posta <posta.erika.mail@gmail.com>
2019-05-03 11:52:31 +0200

    completed string manipulation tasks in TS

--- week-28/day-05/dataStructures.ts
+++ week-28/day-05/dataStructures.ts
@@ -57,6 +57,78 @@ console.log("");
 //SOLUTION FIVE
 // String reverse with Ternary OPerator
 function reverseStringTernaray(str: string): string {
-    return (str === '') ? '' : reverseStringTernaray(str.substr(1)) + str.charAt(0);
-  }
-  console.log(reverseStringTernaray(reversed));
+return (str === '') ? '' : reverseStringTernaray(str.substr(1)) + str.charAt(0);
+}
+console.log(reverseStringTernaray(reversed));
+
+console.log("");
+//TASK 3.
+// Accidentally I got the wrong URL for a funny subreddit. It's probably "odds" and not "bots"
+// Also, the URL is missing a crutial component, find out what it is and insert it too!
+
+let url: string = 'https//www.reddit.com/r/nevertellmethebots';
+console.log(url); //print the original string
+function stringRepair(str: string): string{
+return url.substr(0,5).concat('.').concat(url.substr(5,url.length).replace('bots', 'odds'));
+}
+console.log(stringRepair(url)); // print the corrected url
+
+console.log("");
+//TASK 4
+// While saving this quote, a disk error has occured. Please fix it.
+// Add "always takes longer than" between the words "It" and "you"
+
+let quote: string = `Hofstadter's Law: It you expect, even when you take into account Hofstadter's Law.`;
+console.log(quote);
+function repairQuote(str: string):string{
+    return quote.substr(0,20).concat(" always takes longer than ").concat(quote.substr(21, quote.length));
+}
+
+console.log(repairQuote(quote));
+
+console.log("");
+//TASK 5
+// Add "My todo:" to the beginning of the todoText
+// Add " - Download games" to the end of the todoText
+// Add " - Diablo" to the end of the todoText but with indention
+
+// Expected outpt:
+
+// My todo:
+//  - Buy milk
+//  - Download games
+//      - Diablo
+
+let todoTextOriginal: string = ' - Buy milk\n';
+
+let todoText: string = ' - Buy milk';
+var newTodo: string = "My todo:" + "\n" + todoText + "\n" + ' - Download games' + '\n' + '     - Diablo';
+console.log(newTodo);
+console.log("");
+
+console.log("My todo:\n" + todoTextOriginal.concat(' - Download games\n').concat('     - Diabolo'));
+
+console.log("");
+//TASK 6
+// Things are a little bit messed up
+// Your job is to decode the notSoCrypticMessage by using the hashmap as a look up table
+// Assemble the fragments into the out variable
+
+let out: string = '';
+let notSoCrypticMessage: number[] = [1, 12, 1, 2, 11, 1, 7, 11, 1, 49, 1, 3, 11, 1, 50, 11];
+
+let hashmap = {
+  7: 'run around and desert you',
+  50: 'tell a lie and hurt you ',
+  49: 'make you cry, ',
+  2: 'let you down',
+  12: 'give you up, ',
+  1: 'Never gonna ',
+  11: '\n',
+  3: 'say goodbye '
+};
+
+for(let p = 0; p < notSoCrypticMessage.length; p++){
+    out = out + hashmap[notSoCrypticMessage[p]];
+}
+console.log(out);
